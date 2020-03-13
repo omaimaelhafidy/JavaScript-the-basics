db01ee25454d5736e2148ac17b628f80d0d45dc7
omaimaelhafidy <57996939+omaimaelhafidy@users.noreply.github.com>
2020-03-13 11:09:02 +0100

    la solution

---
+++ app.js
@@ -0,0 +1,23 @@
+function addFavoriteBook(bookName) {
+
+    var favoriteBooks = ["k", "m", "c"];
+    var bookName = favoriteBooks.includes("Great")
+
+    if (bookName == false) {
+        favoriteBooks.push("Great");
+    } else {
+        console.log("It already exists");
+
+    }
+
+    function printFavoritBooks() {
+        var a = favoriteBooks.length;
+        console.log("livres"+" "+"favoris:"+ a);
+        for (let value of favoriteBooks) {
+            console.log(value);
+        }
+
+    }printFavoritBooks()
+
+
+}addFavoriteBook()
